994143fbe840ee286c15e63ac4b2c5b72678882d
udelblue <cdsommers@gmail.com>
2016-09-14 13:33:39 -0400

    add user profile controller

---
+++ src/main/java/com/example/controllers/security/UserProfileController.java
@@ -0,0 +1,36 @@
+package com.example.controllers.security;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.models.security.User;
+import com.example.repository.security.UserRepository;
+
+@Controller
+@RequestMapping(value = "/Account")
+public class UserProfileController {
+
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@RequestMapping(value = "/Profile", method = RequestMethod.GET)
+    @ResponseBody
+    public String myProfile(Principal principal) {
+		User currentUser = userRepository.findByUsername(principal.getName())
+		if (currentUser == null ) {
+			return "";
+		}
+		
+		
+		
+        return"" ;
+    }
+	
+	
+}
