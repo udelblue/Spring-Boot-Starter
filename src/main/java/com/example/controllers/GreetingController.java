6502ea7d233d87e6128def6f031a756ef199dd9c
udelblue <cdsommers@gmail.com>
2016-10-20 12:07:16 -0400

    update

---
+++ src/main/java/com/example/controllers/GreetingController.java
@@ -0,0 +1,22 @@
+package com.example.controllers;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class GreetingController {
+
+	private static final Logger logger = Logger.getLogger( GreetingController.class);
+	
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+	
+}
--- src/main/java/com/example/controllers/HomeController.java
+++ src/main/java/com/example/controllers/HomeController.java
@@ -89,11 +89,6 @@ public class HomeController {
 		
 
 
-	    @RequestMapping("/greeting")
-	    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-	        model.addAttribute("name", name);
-	        return "greeting";
-	    }
 
 
 	
